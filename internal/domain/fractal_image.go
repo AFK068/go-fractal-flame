bc6eb6f91cebe404e8f4ee2eccb572d9743eaa68
ivan <6_6_6_66@mail.ru>
2024-12-07 16:21:49 +0300

    feat: :sparkles: added FractalImage constructor

--- internal/domain/fractal_image.go
+++ internal/domain/fractal_image.go
@@ -4,3 +4,16 @@ type FractalImage struct {
 	Data          []Pixel
 	Width, Height int
 }
+
+func NewFractalImage(width, height int) *FractalImage {
+	data := make([]Pixel, width*height)
+
+	for i := range data {
+		data[i] = Pixel{
+			X: i % width,
+			Y: i / width,
+		}
+	}
+
+	return &FractalImage{data, width, height}
+}
