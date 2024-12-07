27e38039283955187177af25d9286129a3735f61
ivan <6_6_6_66@maik.ru>
2024-12-07 20:56:52 +0300

    feat: :sparkles: added gamma correction method

--- internal/domain/fractal_image.go
+++ internal/domain/fractal_image.go
@@ -1,5 +1,7 @@
 package domain
 
+import "math"
+
 type FractalImage struct {
 	Data          []Pixel
 	Width, Height int
@@ -16,6 +18,34 @@ func NewFractalImage(width, height int) *FractalImage {
 	}
 
 	return &FractalImage{data, width, height}
+}
+
+func (fi *FractalImage) GammaCorrection(gamma float64) {
+	var maxNormal float64
+
+	for row := 0; row < fi.Width; row++ {
+		for col := 0; col < fi.Height; col++ {
+			pixel := fi.Pixel(row, col)
+			if pixel.HitCount != 0 {
+				pixel.Normal = math.Log10(float64(pixel.HitCount))
+				if pixel.Normal > maxNormal {
+					maxNormal = pixel.Normal
+				}
+			}
+		}
+	}
+
+	for row := 0; row < fi.Width; row++ {
+		for col := 0; col < fi.Height; col++ {
+			pixel := fi.Pixel(row, col)
+			if pixel.HitCount != 0 {
+				pixel.Normal /= maxNormal
+				pixel.RGB.R = int(float64(pixel.RGB.R) * math.Pow(pixel.Normal, 1.0/gamma))
+				pixel.RGB.G = int(float64(pixel.RGB.G) * math.Pow(pixel.Normal, 1.0/gamma))
+				pixel.RGB.B = int(float64(pixel.RGB.B) * math.Pow(pixel.Normal, 1.0/gamma))
+			}
+		}
+	}
 }
 
 func (fi *FractalImage) Contains(x, y int) bool {
