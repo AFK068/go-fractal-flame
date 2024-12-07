431ad450f7f6534b3c8116424d1ce15fb9f4d231
ivan <6_6_6_66@mail.ru>
2024-12-07 17:41:45 +0300

    feat: :sparkles: added UpdateColorAndHit method to pixel
    Method that updates pixel parameters using a mutex

--- internal/domain/pixel.go
+++ internal/domain/pixel.go
@@ -1,7 +1,26 @@
 package domain
 
+import "sync"
+
 type Pixel struct {
+	mt       sync.Mutex
 	X, Y     int
 	RGB      RGB
 	HitCount uint64
+}
+
+// Hit increments the hit count of the pixel.
+func (p *Pixel) UpdateColorAndHit(rgb RGB) {
+	p.mt.Lock()
+	defer p.mt.Unlock()
+
+	if p.HitCount == 0 {
+		p.RGB = rgb
+	} else {
+		p.RGB.R = (p.RGB.R + rgb.R) / 2
+		p.RGB.G = (p.RGB.G + rgb.G) / 2
+		p.RGB.B = (p.RGB.B + rgb.B) / 2
+	}
+
+	p.HitCount++
 }
