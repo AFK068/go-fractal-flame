9a57565f59f3baeb7428f698085f1900738f3a9f
ivan <6_6_6_66@mail.ru>
2024-12-08 20:04:58 +0300

    feat: :sparkles: added transformation info structure

---
+++ internal/application/transformations.go
@@ -0,0 +1,10 @@
+package application
+
+import (
+	"github.com/es-debug/backend-academy-2024-go-template/internal/domain"
+)
+
+type TransformationInfo struct {
+	Transformation domain.Transformation
+	Name           string
+}
