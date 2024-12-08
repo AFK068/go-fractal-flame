8fb6a45dd4ed9a61c284c3bef35d72889720b75b
ivan <6_6_6_66@maik.ru>
2024-12-09 01:33:04 +0300

    feat: :loud_sound: added logs

--- internal/application/parameters_config.go
+++ internal/application/parameters_config.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/es-debug/backend-academy-2024-go-template/internal/infrastructure"
 )
@@ -11,8 +12,12 @@ type ParametersConfig struct{}
 func (df *ParametersConfig) InitializeIterations() (iterations float64, err error) {
 	iterations, err = infrastructure.GetValue("Enter iterations (min: 10.0000, max: 3.000.000, recommended: 1.000.000): ", 100000, 3000000)
 	if err != nil {
+		slog.Error("failed to get iterations", slog.String("error", err.Error()))
+
 		return 0, fmt.Errorf("getting iterations: %w", err)
 	}
+
+	slog.Info("iterations initialized", slog.Int("iterations", int(iterations)))
 
 	return iterations, nil
 }
@@ -20,17 +25,25 @@ func (df *ParametersConfig) InitializeIterations() (iterations float64, err erro
 func (df *ParametersConfig) InitializeGamma() (gamma float64, flag bool, err error) {
 	gammaFlag, err := infrastructure.GetTrueOrFalse("Do you want to set gamma?")
 	if err != nil {
+		slog.Error("failed to get gamma flag", slog.String("error", err.Error()))
+
 		return 0, false, fmt.Errorf("getting gamma flag: %w", err)
 	}
 
 	if !gammaFlag {
+		slog.Info("gamma flag initialized", slog.Bool("gamma flag", gammaFlag))
+
 		return 0, false, nil
 	}
 
 	gamma, err = infrastructure.GetValue("Enter gamma (min: 0.1, max: 10, recommended: 2.2): ", 0.1, 10)
 	if err != nil {
+		slog.Error("failed to get gamma", slog.String("error", err.Error()))
+
 		return 0, false, fmt.Errorf("getting gamma: %w", err)
 	}
+
+	slog.Info("gamma initialized", slog.Float64("gamma", gamma))
 
 	return gamma, true, nil
 }
@@ -38,8 +51,12 @@ func (df *ParametersConfig) InitializeGamma() (gamma float64, flag bool, err err
 func (df *ParametersConfig) InitializeSymmetry() (bool, error) {
 	symmetry, err := infrastructure.GetTrueOrFalse("Do you want to set symmetry?")
 	if err != nil {
+		slog.Error("failed to get symmetry", slog.String("error", err.Error()))
+
 		return false, fmt.Errorf("getting symmetry: %w", err)
 	}
+
+	slog.Info("symmetry initialized", slog.Bool("symmetry", symmetry))
 
 	return symmetry, nil
 }
@@ -47,8 +64,12 @@ func (df *ParametersConfig) InitializeSymmetry() (bool, error) {
 func (df *ParametersConfig) InitializeConcurrent() (bool, error) {
 	concurrent, err := infrastructure.GetTrueOrFalse("Do you want to use concurrent mode?")
 	if err != nil {
+		slog.Error("failed to get concurrent", slog.String("error", err.Error()))
+
 		return false, fmt.Errorf("getting concurrent: %w", err)
 	}
+
+	slog.Info("concurrent initialized", slog.Bool("concurrent", concurrent))
 
 	return concurrent, nil
 }
--- internal/application/parameters_initializer.go
+++ internal/application/parameters_initializer.go
@@ -1,6 +1,9 @@
 package application
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+)
 
 type ParametersInitializer interface {
 	InitializeIterations() (iterations float64, err error)
@@ -20,23 +23,40 @@ type Parameters struct {
 func InitializeParameters(initializer ParametersInitializer) (*Parameters, error) {
 	iterations, err := initializer.InitializeIterations()
 	if err != nil {
+		slog.Error("failed to initialize iterations", slog.String("error", err.Error()))
+
 		return nil, fmt.Errorf("initializing iterations: %w", err)
 	}
 
 	gamma, gammaFlag, err := initializer.InitializeGamma()
 	if err != nil {
+		slog.Error("failed to initialize gamma", slog.String("error", err.Error()))
+
 		return nil, fmt.Errorf("initializing gamma: %w", err)
 	}
 
 	symmetry, err := initializer.InitializeSymmetry()
 	if err != nil {
+		slog.Error("failed to initialize symmetry", slog.String("error", err.Error()))
+
 		return nil, fmt.Errorf("initializing symmetry: %w", err)
 	}
 
 	concurrent, err := initializer.InitializeConcurrent()
 	if err != nil {
+		slog.Error("failed to initialize concurrent", slog.String("error", err.Error()))
+
 		return nil, fmt.Errorf("initializing concurrent: %w", err)
 	}
+
+	slog.Info(
+		"parameters initialized successfully",
+		slog.Int("iterations", int(iterations)),
+		slog.Float64("gamma", gamma),
+		slog.Bool("gamma flag", gammaFlag),
+		slog.Bool("symmetry", symmetry),
+		slog.Bool("concurrent", concurrent),
+	)
 
 	return &Parameters{
 		Iterations: iterations,
