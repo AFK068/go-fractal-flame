5206a723eee79cfc4f08412f187c0923bb3ac1e9
ivan <6_6_6_66@maik.ru>
2024-12-08 20:05:32 +0300

    feat: :sparkles: added GetTransformations method

--- internal/application/transformations.go
+++ internal/application/transformations.go
@@ -2,9 +2,28 @@ package application
 
 import (
 	"github.com/es-debug/backend-academy-2024-go-template/internal/domain"
+	transformations "github.com/es-debug/backend-academy-2024-go-template/pkg/nonlinear_transformations"
 )
 
 type TransformationInfo struct {
 	Transformation domain.Transformation
 	Name           string
+}
+
+func GetTransformations() []TransformationInfo {
+	nonLinearTransformations := []TransformationInfo{
+		{&transformations.SinusoidalTransformation{}, "Sinusoidal"},
+		{&transformations.SphericalTransformation{}, "Spherical"},
+		{&transformations.SwirlTransformation{}, "Swirl"},
+		{&transformations.HeartTransformation{}, "Heart"},
+		{&transformations.DiscTransformation{}, "Disc"},
+		{&transformations.ExTransformation{}, "Ex"},
+		{&transformations.FisheyeTransformation{}, "Fisheye"},
+		{&transformations.CosineTransformation{}, "Cosine"},
+		{&transformations.TangentTransformation{}, "Tangent"},
+		{&transformations.NoiseTransformation{}, "Noise"},
+		{&transformations.ExponentialTransformation{}, "Exponential"},
+	}
+
+	return nonLinearTransformations
 }
